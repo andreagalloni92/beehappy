91ba84363132fccbf411531aa58b61e4ea99de54
Andrea Galloni <andreagalloni92@gmail.com>
2021-09-27 14:29:35 +0200

    Generalized startdev  script data path

--- startdev.sh
+++ startdev.sh
@@ -4,19 +4,18 @@ export FLASK_APP=main
 export FLASK_ENV=development
 export FLASK_DEBUG=1
 
-echo $(dirname $0)
-echo "dirname/readlink: $(dirname $(readlink -f $0))"
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  CURR_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$CURR_DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-export DATA_FILE=$DIR'/RPI5_2021_06_10_temp_hum.txt'
+CURR_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/"
+FILE_NAME='RPI5_2021_06_10_temp_hum.txt'
 
-# echo $DATA_FILE
+cd $CURR_DIR
+
+export DATA_FILE=$CURR_DIR$FILE_NAME
 
 flask run
